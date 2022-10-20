96db0fa663df4e114402aada06c7dc9eebf38f41
BlockyPenguin <38588376+ThatBlockyPenguin@users.noreply.github.com>
2022-10-20 18:42:22 +0000

    Add placeholder text on visitor count page

--- visit_check_script.js
+++ visit_check_script.js
@@ -1,6 +1,7 @@
 const url = 'https://api.countapi.xyz/info/mistycountdown.blockypenguin.com/visits'
 
 document.getElementById('message').innerText = 'This site has had';
+document.getElementById('data').innerText = 'Loading...';
 
 function cb(response) {
   document.getElementById('data').innerText = `${response.value} visits!`;
